5c4aa060c1cd40fd7471ed58ab2f17eab4e55d92
adamcread <adamcread1@gmail.com>
2024-02-14 13:26:38 +0000

    move dataset and model

---
+++ model/dataset.py
@@ -0,0 +1,30 @@
+from torch.utils.data import Dataset
+
+class UTKFace(Dataset):
+    def __init__(self, root_dir, device):
+        import torch
+        from torchvision.models import ResNet50_Weights
+        import os
+
+        self.root_dir = root_dir
+        self.device = device
+
+        self.image_paths = os.listdir(self.root_dir)
+
+        self.image_labels = torch.Tensor(
+            [int(x.split("_")[0]) for x in self.image_paths]
+        ).to(torch.float32).to(self.device)
+
+        self.image_transform = ResNet50_Weights.DEFAULT.transforms()
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        from torchvision.io import read_image
+
+        img = read_image(f"{self.root_dir}/{self.image_paths[idx]}")
+        img_tensor = self.image_transform(img).to(self.device)
+        label = self.image_labels[idx].reshape([1])
+
+        return img_tensor, label
