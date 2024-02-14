1ac53e8c7e14afaa5bbf5c9f840a1b9aac9bcf3c
adamcread <adamcread1@gmail.com>
2024-02-14 13:25:19 +0000

    updated folder structure & move to pipenv

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import sys
 import datetime
 
 from dataset import UTKFace
-from model import ResNet50
+from model.model import ResNet50
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
