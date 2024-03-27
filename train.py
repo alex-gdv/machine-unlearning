26fdb7ff1e5b9294e0695f8ba18c36aea9e8931c
alex-gdv <alex-gdv@proton.me>
2024-03-27 15:39:26 +0000

    added test.py

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from model.model import ResNet50Regression
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--experiment", type=str, required=True)
-parser.add_argument("--checkpoint", type=str, required=False)
+parser.add_argument("--checkpoint_epoch", type=str, required=False)
 parser.add_argument("--checkpoint_freq", type=int, default=2)
 parser.add_argument("--val_freq", type=int, default=5)
 parser.add_argument("--epochs", type=int, default=100)
@@ -32,13 +32,13 @@ train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle
 val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)        
 
 os.makedirs(f"./checkpoints/{args.experiment}", exist_ok=True)
-if args.checkpoint is None:
+if args.checkpoint_epoch is None:
     model = ResNet50Regression()
     optimizer = torch.optim.Adam(model.parameters())
 
     start_epoch = 0
 else:
-    checkpoint = torch.load(args.checkpoint)
+    checkpoint = torch.load(f"./checkpoints/{args.experiment}/epoch_{args.checkpoint_epoch}.pt")
 
     model = ResNet50Regression().load_state_dict(checkpoint["model_state_dict"])
     optimizer = torch.optim.Adam().load_state_dict(checkpoint["optimizer_state_dict"])
