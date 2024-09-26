b36b36cd65cc6d173a06a7a00b908288c5622407
Adam Read <adamcread1@gmail.com>
2024-09-26 17:21:13 +0100

    continued refactor almost complete

--- unrolling-sgd/gradient_ascent.py
+++ unrolling-sgd/gradient_ascent.py
@@ -1,13 +1,11 @@
+from PyHessian.pyhessian import hessian
 from torch.utils.data import DataLoader
-import torch.backends.cudnn as cudnn
 import torch.nn.functional as F
 import torch.optim as optim
-import torch.nn as nn
+import numpy as np
 import torch
 import argparse
 import copy
-import numpy as np
-from PyHessian.pyhessian import hessian
 import os
 
 from model.dataset import UTKFaceRegression
@@ -24,20 +22,11 @@ def weights_to_list_fast(weights):
     return weights_list
 
 
-def std_loss(x,y):
-    log_prob = -1.0 * F.log_softmax(x, 1)
-    loss = log_prob.gather(1, y.unsqueeze(1))
-    loss = loss.mean()
-    avg_std = torch.sum(torch.std(x, dim=1))/(len(x.view(-1)))
-    loss = loss + std_reg*avg_std
-    return loss
-
-std_reg =1.0
+# todo: thin these down
+std_reg = 1.0
 parser = argparse.ArgumentParser(description='Finetuning for verification error')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-
 parser.add_argument('--model', default='resnet', type=str, help='resnet or vgg')
-parser.add_argument('--loss_func', default='regular', type=str, help='loss function: regular,hessian, hessianv2, std_loss')
 parser.add_argument('--dataset', default = 'cifar10', type=str, help ='cifar10, cifar100')
 parser.add_argument('--pretrain_epochs', default=10, type=int, help='number of pretraining epochs')
 parser.add_argument('--pretrain_batch_size', default=32, type=int, help='pretraining batch size')
@@ -50,174 +39,94 @@ args = parser.parse_args()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 train_dataset = UTKFaceRegression("data/retain.json")
+forget_dataset = UTKFaceRegression("data/forget.json")
 val_dataset = UTKFaceRegression("data/val.json")
 
 train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)   
+forget_dataloader = DataLoader(forget_dataset, batch_size=args.batch_size, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
 
-print('==> Preparing Hessian data..')
-# ! UNDERSTAND WHAT THIS IS BEFORE CONTINUING WITH IT
-# trainset_list = list(trainset)
-# hessian_loader = trainset_list[:512]
-# hessian_loader = torch.utils.data.DataLoader(hessian_loader, batch_size=512, shuffle=False, num_workers=2)
+# todo: ensure that the train dataset is shuffled before we select images for hessian
+hessian_loader = train_dataset[:512] # select random 512 images (random bc previously shuffled)
+hessian_loader = torch.utils.data.DataLoader(hessian_loader, batch_size=512, shuffle=False, num_workers=2) # put in dataloader
 
-# print('==> Preparing Finetuning data...')
-# batch_star = trainset_list[args.finetune_batch_size * args.unlearn_batch: args.finetune_batch_size * (args.unlearn_batch+1)]
-# data_no_unlearned = trainset_list[:args.finetune_batch_size * args.unlearn_batch] + trainset_list[args.finetune_batch_size * (args.unlearn_batch+1):]
-# unlearned_loader = torch.utils.data.DataLoader(batch_star, batch_size=args.finetune_batch_size, shuffle=False, num_workers=2)
+# get model from best checkpoint
+model_pretrained = ResNet50Regression()
+model_pretrained = model_pretrained.to(device)
+checkpoint = torch.load(
+    f"./checkpoints/{args.experiment}/epoch_best.pt", 
+    map_location=device
+)
+model_pretrained.load_state_dict(checkpoint["model_state_dict"])
+criterion = torch.nn.MSELoss()
 
+# todo: this is only one batch in the original implementation, how can we extend it to multiple batches?
+# get gradients of the pretrained model on forget set
+for X, y_true in enumerate(forget_dataloader):
+    y_true = y_true.to(device)
+    X = X.to(device)
+    
+    y_pred = model_pretrained(X)
+    forget_loss = criterion(y_pred, y_true)
+    
+    forget_loss.backward(retain_graph=True)
+    forget_grads = torch.autograd.grad(forget_loss, [param for param in model_pretrained.parameters()], create_graph = True)
 
-#Getting model
-model = ResNet50Regression()
-model = model.to(device)
+# todo: add lr scheduler back
+model = copy.deepcopy(model_pretrained)
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
-os.makedirs(f"./checkpoints/{args.experiment}", exist_ok=True)
-if args.checkpoint_epoch is None:
-    optimizer = torch.optim.Adam(model.parameters())
-    start_epoch = 0
-else:
-    checkpoint = torch.load(
-        f"./checkpoints/{args.experiment}/epoch_{args.checkpoint_epoch}.pt", 
-        map_location=device
-    )
-    model.load_state_dict(checkpoint["model_state_dict"])
-    optimizer = torch.optim.Adam(model.parameters())
-    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-
-    start_epoch = checkpoint["epoch"] + 1
-
-# ! WHAT'S THE POINT OF THIS?
-#saving the weights of the pretrained model
-M_pretrain = copy.deepcopy(model)
-w_pretrain_weights_tensor = [param for param in M_pretrain.parameters()]
-w_pretrain_weights = weights_to_list_fast(w_pretrain_weights_tensor)
-
-print('==> Beginning iteration over T=0 to T=500...')
-data_ret = {}
-
-# ! DON'T THINK WE NEED TWO MODELS
-#1. Initialize your 2 models: M and M'
-M = copy.deepcopy(M_pretrain)
-
-M.train()
-
-#initialize the loss functions, optimizers and schedulers for both models
-optimizer = optim.SGD(M.parameters(), lr=args.lr,momentum=0.9, weight_decay=5e-4)
-criterion = torch.nn.MSELoss(reduction="sum")
-
-# ! NO LR SCHEDULER TO BEGIN WITH - ADD THIS BACK LATER
-# scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200) 
-
-#the data that we are finetuning on (with x* at the beginning)
-# ! FIGURE OUT HOW TO WRANGLE WITH THIS
-data = batch_star + data_no_unlearned
-data_loader = torch.utils.data.DataLoader(data, batch_size = args.finetune_batch_size, shuffle = False, num_workers = 2)
-
-sigma_list  = []
-print('T = ',len(data_loader))
-
-#we need some lists for statistics
 sigma_list = []
 delta_weights_list = []
-rolling_unl_error_list = [0]
-ver_error_list = []
+rolling_unl_error_list = [0.]
 
-for ep in range(args.finetune_epochs):
-    for main_idx,(inputs, targets) in enumerate(data_loader):
-        print('Epoch: ',ep, 't:',main_idx)
-        actual_idx = (ep*len(data_loader)) + main_idx
-        inputs, targets = inputs.to(device), targets.to(device)
-        M.train()
+for epoch in range(0, args.finetune_epochs):
+    for batch, (X, y_true) in enumerate(train_dataloader):
+        print(f"epoch {epoch} batch {batch}")
+        y_true = y_true.to(device)
+        X = X.to(device)
+        model.train()
         
-        if (actual_idx!=0) and (actual_idx)%args.checkpoint_freq ==0:
-            M_interim = copy.deepcopy(M)
+        y_pred = model(X)
         
         optimizer.zero_grad()
-        outputs_M = M(inputs)
-        loss_M = criterion(outputs_M, targets)
+        loss = criterion(y_pred, y_true)
 
-        loss_M.backward()
+        loss.backward()
         optimizer.step()
-        curr_weights = w_pretrain_weights
-        curr_error = 0
-        M_interim = copy.deepcopy(M_pretrain)
 
-        M.eval()
-        #print('==> Getting Unlearning error...')
-
+        # todo: what is this? What do eigenvalues and eigenvectors do here?
+        model.eval()
         for i,(img,label) in enumerate(hessian_loader):
             img = img.cuda()
             label = label.cuda()
             break
-        if args.loss_func == 'regular':
-            hessian_comp = hessian(M, hessian_criterion, data=(img, label), cuda=True)
-        if args.loss_func == 'std':
-            hessian_comp = hessian(M, std_loss, data=(img, label), cuda=True)
+        hessian_comp = hessian(model, criterion, data=(img, label), cuda=True)
         top_eigenvalues, top_eigenvector = hessian_comp.eigenvalues()
         sigma = np.sqrt(top_eigenvalues[-1])
         sigma_list.append(sigma)
         
-        #very important: if you are now at the start of a new checkpoint, you want to update the weights to compare to:
-        #if main_idx!=0 and main_idx%args.checkpoint_freq ==0:
-        if (actual_idx!=0) and (actual_idx)%args.checkpoint_freq ==0:
-            curr_weights = w_M_weights
-            curr_error = rolling_unl_error
-        
-        #Now, save the weights of both M_(N+t) and M'_(N+t)
-        M_weights_tensor = [param for param in M.parameters()]
+        # save model weights
+        M_weights_tensor = [param for param in model.parameters()]
         w_M_weights = weights_to_list_fast(M_weights_tensor)
-
-        M_retrain_tensor = [param for param in M_retrained.parameters()]
-        w_M_retrain_weights = weights_to_list_fast(M_retrain_tensor)
-
-        #Now, get M''_(N+t)
-        M_unlearned = copy.deepcopy(M)
-        optimizer_unlearned = optim.SGD(M_unlearned.parameters(), lr=args.lr,momentum=0.9, weight_decay=5e-4)
-        scheduler_unlearned = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_unlearned, T_max=200)
-
-        M_interim.train()
-
-        for i,(img,label) in enumerate(unlearned_loader):
-            img = img.cuda()
-            label = label.cuda()
-            output_pre = M_interim(img)
-            if args.loss_func == 'regular':
-                loss_unl = criterion(output_pre, label)
-            if args.loss_func == 'std':
-                loss_unl = std_loss(output_pre,label)
-            loss_unl.backward(retain_graph=True)
-            grads = torch.autograd.grad(loss_unl, [param for param in M_interim.parameters()],create_graph = True)
+        
+        # todo: add lr scheduler back
+        # why is this redefined as model each time? is that part of why we have checkpoints?
+        model_unlearned = copy.deepcopy(model)
+        optimizer_unlearned = optim.SGD(model_unlearned.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+        
         old_params = {}
-        for i, (name, params) in enumerate(M.named_parameters()):
+        for i, (name, params) in enumerate(model.named_parameters()):
             old_params[name] = params.clone()
-            old_params[name] += (ep+1) * args.lr * grads[i]
-        for name, params in M_unlearned.named_parameters():
+            old_params[name] += (epoch+1) * args.lr * forget_grads[i]    
+        for name, params in model_unlearned.named_parameters():
             params.data.copy_(old_params[name])
-        M_unlearned_tensor = [param for param in M_unlearned.parameters()]
-        w_M_unlearned_weights = weights_to_list_fast(M_unlearned_tensor)
-
             
+        M_unlearned_tensor = [param for param in model_unlearned.parameters()]
+        w_M_unlearned_weights = weights_to_list_fast(M_unlearned_tensor)
+        
+        # todo: what are these intended to store?
         delta_weights = np.linalg.norm((np.array(w_M_weights) - np.array(curr_weights)))
-        t_num = actual_idx%args.checkpoint_freq
-        if main_idx ==0 and ep ==0:
-            rolling_unl_error = 0
-        if actual_idx%args.checkpoint_freq ==0 and actual_idx !=0:
-            rolling_error = curr_error
-        if actual_idx%args.checkpoint_freq != 0:
-            rolling_unl_error = curr_error+ (args.lr * args.lr) * (t_num) * delta_weights * (sum(sigma_list[-t_num:])/len(sigma_list[-t_num:]))
-
-
+        t_num = len(train_dataloader)*epoch + batch  
+        rolling_unl_error = curr_error + (args.lr * args.lr) * (t_num) * delta_weights * (sum(sigma_list[-t_num:])/len(sigma_list[-t_num:]))
         rolling_unl_error_list.append(rolling_unl_error)
-
-
-ret = {}
-ret['sigma'] = sigma_list
-ret['rolling unlearning error'] = rolling_unl_error_list
-
-
-import pickle
-if not os.path.isdir('final_checkpoint_correlation_results'):
-    os.mkdir('final_checkpoint_correlation_results')
-path = f'./final_checkpoint_correlation_results/{args.model}_{args.dataset}_{args.loss_func}_{args.pretrain_batch_size}_{args.pretrain_epochs}_{args.finetune_batch_size}_{args.finetune_epochs}_{args.checkpoint_freq}.p'
-
-pickle.dump(ret, open(path, 'wb'))
