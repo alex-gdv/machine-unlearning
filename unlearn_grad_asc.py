1e166bd7a25da0f6fb8cbf2a78128f7ba3f2d961
Adam Read <adamcread1@gmail.com>
2024-09-28 12:44:32 +0100

    fully cleaned gradient ascent code

--- unlearn_grad_asc.py
+++ unlearn_grad_asc.py
@@ -1,4 +1,3 @@
-from PyHessian.pyhessian import hessian
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import torch.optim as optim
@@ -10,16 +9,6 @@ import random
 
 from model.dataset import UTKFaceRegression
 from model.model import ResNet50Regression
-
-
-def weights_to_list_fast(weights):
-  with torch.no_grad():
-    weights_list = []
-    for weight in weights:
-      list_t = weight.view(-1).tolist()
-      weights_list = weights_list + list_t
-
-    return weights_list
 
 
 # todo: thin these down
@@ -38,17 +27,19 @@ args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+# gather training and val datasets
 train_dataset = UTKFaceRegression("data/retain.json")
-forget_dataset = UTKFaceRegression("data/forget.json")
 val_dataset = UTKFaceRegression("data/val.json")
-
 train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-forget_dataloader = DataLoader(forget_dataset, batch_size=args.batch_size, shuffle=True)
 val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
 
-# todo: ensure that the train dataset is shuffled before we select images for hessian
-hessian_loader = train_dataset[:512] # select random 512 images (random bc previously shuffled)
-hessian_loader = torch.utils.data.DataLoader(hessian_loader, batch_size=512, shuffle=False, num_workers=2) # put in dataloader
+# gather forget dataset
+forget_dataset = UTKFaceRegression("data/forget.json")
+forget_dataloader = DataLoader(forget_dataset, batch_size=len(forget_dataset), shuffle=True)
+
+# randomly select a subset of images to gather hessian data for unlearning loss
+hessian_loader = random.sample(train_dataset, k=512)
+hessian_loader = torch.utils.data.DataLoader(hessian_loader, batch_size=512, shuffle=False, num_workers=2)
 
 # get model from best checkpoint
 model_pretrained = ResNet50Regression()
@@ -60,7 +51,6 @@ checkpoint = torch.load(
 model_pretrained.load_state_dict(checkpoint["model_state_dict"])
 criterion = torch.nn.MSELoss()
 
-# todo: this is only one batch in the original implementation, how can we extend it to multiple batches?
 # get gradients of the pretrained model on forget set
 for X, y_true in enumerate(forget_dataloader):
     y_true = y_true.to(device)
@@ -72,13 +62,8 @@ for X, y_true in enumerate(forget_dataloader):
     forget_loss.backward(retain_graph=True)
     forget_grads = torch.autograd.grad(forget_loss, [param for param in model_pretrained.parameters()], create_graph = True)
 
-# todo: add lr scheduler back
 model = copy.deepcopy(model_pretrained)
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-
-sigma_list = []
-delta_weights_list = []
-rolling_unl_error_list = [0.]
 
 for epoch in range(0, args.finetune_epochs):
     for batch, (X, y_true) in enumerate(train_dataloader):
@@ -94,39 +79,12 @@ for epoch in range(0, args.finetune_epochs):
 
         loss.backward()
         optimizer.step()
-
-        # todo: what is this? What do eigenvalues and eigenvectors do here?
-        model.eval()
-        for i,(img,label) in enumerate(hessian_loader):
-            img = img.cuda()
-            label = label.cuda()
-            break
-        hessian_comp = hessian(model, criterion, data=(img, label), cuda=True)
-        top_eigenvalues, top_eigenvector = hessian_comp.eigenvalues()
-        sigma = np.sqrt(top_eigenvalues[-1])
-        sigma_list.append(sigma)
         
-        # save model weights
-        M_weights_tensor = [param for param in model.parameters()]
-        w_M_weights = weights_to_list_fast(M_weights_tensor)
-        
-        # todo: add lr scheduler back
-        # why is this redefined as model each time? is that part of why we have checkpoints?
-        model_unlearned = copy.deepcopy(model)
-        optimizer_unlearned = optim.SGD(model_unlearned.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-        
+        # apply gradient ascent for unlearning
+        model_unlearned = copy.deepcopy(model)        
         old_params = {}
         for i, (name, params) in enumerate(model.named_parameters()):
             old_params[name] = params.clone()
             old_params[name] += (epoch+1) * args.lr * forget_grads[i]    
         for name, params in model_unlearned.named_parameters():
             params.data.copy_(old_params[name])
-            
-        M_unlearned_tensor = [param for param in model_unlearned.parameters()]
-        w_M_unlearned_weights = weights_to_list_fast(M_unlearned_tensor)
-        
-        # todo: what are these intended to store?
-        delta_weights = np.linalg.norm((np.array(w_M_weights) - np.array(curr_weights)))
-        t_num = len(train_dataloader)*epoch + batch  
-        rolling_unl_error = curr_error + (args.lr * args.lr) * (t_num) * delta_weights * (sum(sigma_list[-t_num:])/len(sigma_list[-t_num:]))
-        rolling_unl_error_list.append(rolling_unl_error)
