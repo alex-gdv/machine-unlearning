ede0851fc978df8d4cbde6147dcf6d6f91fa5ff6
alex-gdv <alex-gdv@proton.me>
2025-07-06 14:10:09 +0100

    Adapt memory selector to regression

--- kpriors/memory_selector.py
+++ kpriors/memory_selector.py
@@ -1,58 +1,48 @@
-"""Source:
-@article{khan2021knowledge,
-  title = {Knowledge-Adaptation Priors},
-  author = {Khan, Mohammad Emtiyaz and Swaroop, Siddharth},
-  journal = {Advances in Neural Information Processing Systems},
-  year = {2021}
-}
-GitHub:
-https://github.com/team-approx-bayes/kpriors
-
-"""
+"""Code adapted from https://github.com/team-approx-bayes/kpriors."""
 
 import torch
-import torch.nn.functional as F
-import numpy as np
-
-
-# what is the role of the softmax function here???
-def softmax_jacobian(f):
-    s = F.softmax(f, dim=-1)
-    return s - s*s
 
 
 def select_memory_points(dataloader, model, memory_size, device):
-    with torch.no_grad():
-        model.to(device)
+    top_norms = torch.Tensor([], device="cpu")
+    top_inputs = torch.Tensor([]).to(device)
+    top_outputs = torch.Tensor([]).to(device)
 
-        top_scores = torch.Tensor([], device="cpu")
-        top_inputs = torch.Tensor([]).to(device)
-        top_labels = torch.Tensor([]).to(device)
-        top_soft_labels = torch.Tensor([]).to(device)
+    criterion = torch.nn.MSELoss(reduction="none")
 
-        for batch, (inputs, labels) in enumerate(dataloader):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
+    for inputs, labels in dataloader:
+        inputs = inputs.to(device)
+        labels = labels.to(device).float()
 
-            outputs = model(inputs)
-            lamb = softmax_jacobian(outputs)
-            if device.type == "cuda":
-                lamb = lamb.cpu()
-            lamb = lamb.detach()
-            top_scores = torch.cat([top_scores, lamb])
-            top_inputs = torch.cat([top_inputs, inputs])
-            top_labels = torch.cat([top_labels, labels])
-            top_soft_labels = torch.cat([top_soft_labels, outputs])
-            if top_scores.shape[0] > memory_size:
-                _, indices = top_scores.sort(descending=True)
-                top_scores = top_scores[indices[:memory_size]]
-                top_inputs = top_inputs[indices[:memory_size]]
-                top_labels = top_labels[indices[:memory_size]]
-                top_soft_labels = top_soft_labels[indices[:memory_size]]
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        
+        norms = []
+        for i in range(loss.size()[0]):
+            model.zero_grad()
+            loss[i].backward(retain_graph=True)
 
-        memory = {}
-        memory["inputs"] = top_inputs.to(device)
-        memory["labels"] = top_labels.to(device)
-        memory["soft_labels"] = top_soft_labels.to(device)
+            grad_vector = torch.cat([
+                p.grad.flatten() for p in model.parameters() if p.grad is not None
+            ])
 
-        return memory
+            norm = torch.norm(grad_vector, p=2)
+            norms.append(norm)
+
+        norms = torch.Tensor(norms, device="cpu")
+        top_norms = torch.cat([top_norms, norms])
+        top_inputs = torch.cat([top_inputs, inputs])
+        top_outputs = torch.cat([top_outputs, outputs])
+
+        if top_norms.size()[0] > memory_size:
+            print("hello")
+            _, indices = top_norms.sort(descending=True)
+            top_norms = top_norms[indices[:memory_size]]
+            top_inputs = top_inputs[indices[:memory_size]]
+            top_outputs = top_outputs[indices[:memory_size]]
+
+    memory = {}
+    memory["inputs"] = top_inputs
+    memory["outputs"] = top_outputs
+
+    return memory
